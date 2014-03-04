4a1c0c6a179ba697efb30643845ab53a1c6ef7b2
ypsilon.takai <ypsilon.takai@gmail.com>
2014-03-05 03:58:13 +0900

    put image info into db

--- picture-gallery/src/picture_gallery/models/db.clj
+++ picture-gallery/src/picture_gallery/models/db.clj
@@ -21,3 +21,16 @@
     (first res)))
 
 
+(defn add-image [userid name]
+  (with-db
+    sql/transaction
+    (if (sql/with-query-results res
+          ["select userid from images where userid = ? and name = ?" userid name]
+          (empty? res))
+      (sql/insert-record :images {:userid userid :name name})
+      (throw
+       (Exception. "you have already uploaded an image with the same name")))))
+
+
+
+
